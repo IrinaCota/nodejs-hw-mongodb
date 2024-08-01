98b3302b0af522088faad82610762b26e833172c
IrinaCota <iryna.kotsa91@gmail.com>
2024-08-01 19:41:31 +0100

    task 2

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+const PORT = Number(('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use(cors());
+
+  app.use('*', (req, res) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
