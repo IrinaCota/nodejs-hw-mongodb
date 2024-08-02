cb916f47c2adc40dc78576af89fc3612fd1a53ac
IrinaCota <iryna.kotsa91@gmail.com>
2024-08-02 21:36:37 +0100

    task 1

---
+++ src/routers/contacts.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import {
+  getContactByIdController,
+  getStudentsController,
+} from '../controllers/contacts.js';
+
+const router = Router();
+
+router.get('/contacts', getStudentsController);
+
+router.get('/contacts/:contactId', getContactByIdController);
+
+export default router;
