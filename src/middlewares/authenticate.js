c3ad9438cf98690936ad31fc88cdc6f1341cb4b1
IrinaCota <iryna.kotsa91@gmail.com>
2024-08-26 21:48:30 +0100

    authenticate-step7

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,3 +1,37 @@
 import createHttpError from 'http-errors';
 import { SessionCollection } from '../db/models/session.js';
 import { UserCollection } from '../db/models/user.js';
+
+export const authenticate = async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return next(createHttpError(401, 'Provide Authorization header'));
+  }
+
+  const [ bearer, token ] = authorization.split(' ');
+
+  if (bearer !== 'Bearer' || !token) {
+    return next(createHttpError(401, 'Auth header should be of type Bearer'));
+  }
+
+  const session = await SessionCollection.findOne({ accessToken: token });
+
+  if (!session) {
+    return next(createHttpError(401, 'Session not found'));
+  }
+
+  if (new Date() > new Date(session.accessTokenValidUntil)) {
+    return next(createHttpError(401, 'Access token is expired'));
+  }
+
+  const user = await UserCollection.findById(session.userId);
+
+  if (!user) {
+    return next(createHttpError(401));
+  }
+
+  req.user = user;
+
+  next();
+};
